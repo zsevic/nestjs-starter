b3e1a1d999767885cb232a972f01771560959d49
zsevic <zsevic95@gmail.com>
2021-05-20 01:46:00 +0200

    Update API documentation setup

--- src/common/config/api-docs/constants.ts
+++ src/common/config/api-docs/constants.ts
@@ -1,4 +1,6 @@
-export const SWAGGER_API_NAME = 'nestjs-starter backend';
-export const SWAGGER_API_DESCRIPTION = 'nestjs-starter API documentation';
-export const SWAGGER_API_CURRENT_VERSION = '0.0.1';
+import { name, description, version } from 'root/package.json';
+
+export const SWAGGER_API_NAME = name;
+export const SWAGGER_API_DESCRIPTION = description;
+export const SWAGGER_API_CURRENT_VERSION = version;
 export const SWAGGER_API_ROOT = 'api-docs';
